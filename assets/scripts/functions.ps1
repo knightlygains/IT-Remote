2eafc77b3a875889838b03efa1746cf403939d9a
Steven Whitney <56776962+knightlygains@users.noreply.github.com>
2024-06-11 11:48:36 -0700

    Simplify winrm process

--- assets/scripts/functions.ps1
+++ assets/scripts/functions.ps1
@@ -6,27 +6,18 @@ Function Enable-WinRM {
         [string]$winRM = $true
     )
 
-    Write-Host "winRM is $winRM"
-
     if ($winRM -eq $true) {
-        Write-Host "Enabling WinRM on" $Computer "..." -ForegroundColor red
-        psexec.exe \\"$Computer" -s -nobanner -accepteula C:\Windows\System32\winrm.cmd qc -quiet
-    
-        $result = winrm id -r:$Computer 2>$null
-        if ($LastExitCode -eq 0) {
-            psservice.exe \\"$Computer" -nobanner restart WinRM
-            # $result = winrm id -r:$Computer 2>$null
-            if ($LastExitCode -eq 0) { 
+        psexec.exe \\$Computer -s -nobanner -accepteula powershell Enable-PSRemoting -Force
+
+        try {
+            $winrm_service = Invoke-Command -ComputerName $Computer -ScriptBlock {
+                Get-Service winrm
+            } -ErrorAction Stop
+            if ( $winrm_service.Status -eq "Running") {
                 return $true
             }
-            else {
-                return $false
-            }
-            while ($true) {
-                continue
-            }
         }
-        else {
+        catch {
             return $false
         }
     }
